3ea8e0619c3209319c4c0bf02edb445eb6eb472f
AlekseyGurev <gurevaleksey@yandex.ru>
2023-10-10 11:33:56 +0300

    добавляет правки наставника

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,5 @@
-const fitsLength = (string,symbols) => {
-  if (string.length <= symbols){
+const fitsLength = (text,lengthText) => {
+  if (text.length <= lengthText){
     return true;
   }
   return false;
@@ -9,36 +9,27 @@ fitsLength('проверяемая строка', 20);
 fitsLength('проверяемая строка', 18);
 fitsLength('проверяемая строка', 10);
 
-const checkPalindrome = (string) => {
+const isPalindrome = (string) => {
   string = string.toLowerCase().replaceAll(' ', '');
-
-  if (string === string.split('').reverse().join('')){
-    return true;
-  }
-  return false;
+  return string === string.split('').reverse().join('');
 };
 
-checkPalindrome('топот'); // true
-checkPalindrome('ДовОд'); // true
-checkPalindrome('Кекс'); // false
-checkPalindrome('Лёша на полке клопа нашёл '); // true
+isPalindrome('топот'); // true
+isPalindrome('ДовОд'); // true
+isPalindrome('Кекс'); // false
+isPalindrome('Лёша на полке клопа нашёл '); // true
 
-const searchNumbers = (data) => {
-  const result = [];
-  data.toString().split('').forEach((element) => {
-    if (!Number.isNaN(parseInt(element,10))) {
-      result.push(element);
-    }
-  });
+const parseDigits = (data) => {
+  const result = data.toString().split('').filter((element)=> (!Number.isNaN(parseInt(element,10))));
   return result ? parseInt(result.join(''), 10) : NaN;
 };
 
-searchNumbers('2023 год'); // 2023
-searchNumbers('ECMAScript 2022'); // 2022
-searchNumbers('1 кефир, 0.5 батона'); // 105
-searchNumbers('агент 007'); // 7
-searchNumbers('а я томат'); // NaN
-searchNumbers(2023); // 2023
-searchNumbers(-1); // 1
-searchNumbers(1.5); // 15
+parseDigits('2023 год'); // 2023
+parseDigits('ECMAScript 2022'); // 2022
+parseDigits('1 кефир, 0.5 батона'); // 105
+parseDigits('агент 007'); // 7
+parseDigits('а я томат'); // NaN
+parseDigits(2023); // 2023
+parseDigits(-1); // 1
+parseDigits(1.5); // 15
 
