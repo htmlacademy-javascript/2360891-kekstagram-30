50086f3a828555c11d3f34a696765250ee428224
AlekseyGurev <gurevaleksey@yandex.ru>
2023-10-19 08:51:04 +0300

    добавляет модули util.js и data.js

---
+++ js/util.js
@@ -0,0 +1,25 @@
+const getRandomItem = (items) => {
+  const fraction = items.length * Math.random();
+  const index = Math.floor(fraction);
+  return items[index];
+};
+
+const getRandomInteger = (min,max) => {
+  const fraction = (max - min) * Math.random() + min;
+  return Math.round(fraction);
+};
+
+const getRandomMessage = (messages) => {
+  let result = '';
+  let count = getRandomInteger(1, 2);
+  while (count) {
+    const message = getRandomItem(messages);
+    if (!result.includes(message)) {
+      result += message;
+      count--;
+    }
+  }
+  return result;
+};
+
+export{getRandomItem, getRandomInteger, getRandomMessage};
