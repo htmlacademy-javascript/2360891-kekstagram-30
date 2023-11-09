ff9771d45b087d21096c1bae0b7b5cc21848125f
AlekseyGurev <gurevaleksey@yandex.ru>
2023-11-09 14:18:04 +0300

    рефакторинг кода в файле status

--- js/status.js
+++ js/status.js
@@ -5,20 +5,16 @@ const renderStatus = (id) => {
   const statusElement = template.content.querySelector(`.${id}`).cloneNode(true);
   document.body.append(statusElement);
 
-  const removeElement = () => {
-    document.querySelector(`.${id}`).remove();
-  };
-
   if (id === 'data-error') {
     setTimeout(() => {
-      removeElement(id);
+      statusElement.remove();
     }, ALERT_SHOW_TIME);
   }else {
     const button = document.querySelector(`.${id}__button`);
     statusElement.addEventListener('click', (event) => {
       if((event.target === statusElement) || (event.target === button)){
         document.dispatchEvent(new CustomEvent('closeStatus', {detail: id}));
-        removeElement();
+        statusElement.remove();
       }
     });
   }
