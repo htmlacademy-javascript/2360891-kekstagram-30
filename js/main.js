9b4be5bae2464afcd116e648738694dc75848cd1
AlekseyGurev <gurevaleksey@yandex.ru>
2023-11-15 11:52:20 +0300

    дз 12/3

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ import {resetForm, setSubmitDisabled} from'./upload/upload.js';
 import {renderStatus} from './status.js';
 
 const DELAY = 5000;
-const BASE_URL = 'https://30.javascript.pages.academy/kekstagram';
+const BASE_URL = 'https://30.javascript.pages.academy/kekstagram/';
 
 document.addEventListener('formdata', async (event) => {
   try {
@@ -19,7 +19,7 @@ document.addEventListener('formdata', async (event) => {
   }
 });
 
-request((`${BASE_URL}/data`), {method:'GET'})
+request((`${BASE_URL}data`), {method:'GET'})
   .then((data) => renderGallery(data, debounce))
   .catch(() => renderStatus('data-error', {autoHide: DELAY}));
 
